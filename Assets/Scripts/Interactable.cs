538c7458cc7a7eea052380c4c67fa174fae6fe30
Aurilux <primordias@gmail.com>
2024-08-17 17:32:10 -0700

    Basic character movement and interaction

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    // Start is called before the first frame update
+    [SerializeField] public bool isJekyllInteractable = false;
+    [SerializeField] public bool isHydeInteractable = false;
+}
